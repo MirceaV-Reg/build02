82df35625bf54f1e42eb17d75124b94904a6db8e
Mircea Voina <mircea.voina@regnology.net>
2025-08-07 07:35:04 +0000

    add real unit test

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,7 +1,8 @@
 public class HelloWorld {
 public static void main(String[] args) {
-// Prints "Hello, World again" to the terminal window.
-System.out.println("Hello, World");
+// Prints "Hello, World" to the terminal window.
+HelloWorldHelper helper = new HelloWorldHelper ();
+System.out.println(helper.useEnglish());
 }
 }
 
